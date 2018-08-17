fbe00f0b4492bcb855894788250a445706523d2c
zws910 <vincentzheng0910@gmail.com>
2018-08-17 11:27:35 +0800

    move app initial to info.__init__.py

--- manager.py
+++ manager.py
@@ -1,24 +1,8 @@
-from flask import Flask, session
+from flask import session
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.wtf import CSRFProtect
-from redis import StrictRedis
-# 可以用来指定 session 保存的位置
-from flask_session import Session
-from config import Config
 
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(Config)
-# 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启当前项目 CSRF 保护, 只做服务器验证功能
-CSRFProtect(app)
-# 设置session保存位置
-Session(app)
+from info import app, db
 
 manager = Manager(app)
 # 将app和db关联
@@ -30,7 +14,7 @@ manager.add_command('db', MigrateCommand)
 @app.route('/')
 def index():
     session["name"] = "itheima"
-    return 'Hello Index233!'
+    return 'Hello Index222!'
 
 
 if __name__ == '__main__':
