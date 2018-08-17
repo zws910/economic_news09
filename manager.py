2d4cf6cf5207b49524a2695fa6736657858633b9
zws910 <vincentzheng0910@gmail.com>
2018-08-17 09:21:34 +0800

    Project config

--- manager.py
+++ manager.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    """为MySQL数据库添加配置"""
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
