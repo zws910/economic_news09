b4c191fd79e58153d2ddd797effb8e270ee075d3
zws910 <vincentzheng0910@gmail.com>
2018-08-19 09:44:49 +0800

    add index.html and load favicon

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,6 @@
+from flask import current_app
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
@@ -7,4 +10,10 @@ def index():
     # 向redis中保存一个值
     redis_store.set("name", "itnimei")
 
-    return 'Hello Index!'
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
+
