e0f6e30a39302981216a4a4d91d148435db6776e
zws910 <vincentzheng0910@gmail.com>
2018-08-23 15:55:39 +0800

    user info page -- get username from database

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,9 +1,17 @@
 from flask import g
 from flask import redirect
 from flask import render_template
+from flask import request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+
+@profile_blu.route('/base_info', methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
 
 
 @profile_blu.route('/info')
@@ -15,8 +23,5 @@ def user_info():
         return redirect("/")
 
     data = {"user": user.to_dict()}
-
-
-
 
     return render_template('news/user.html', data=data)
