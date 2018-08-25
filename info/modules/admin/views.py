41ef33621c827ebb143ef674e5a802f33d01e808
zws910 <vincentzheng0910@gmail.com>
2018-08-25 17:15:49 +0800

    add test users

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from flask import g
 from flask import redirect
 from flask import render_template
 from flask import request
@@ -7,11 +8,15 @@ from flask import url_for
 
 from info.models import User
 from info.modules.admin import admin_blu
+from info.utils.common import user_login_data
 
 
 @admin_blu.route('/index')
+@user_login_data
 def index():
-    return render_template('admin/index.html')
+    user = g.user
+
+    return render_template('admin/index.html', user=user.to_dict())
 
 
 @admin_blu.route('/login', methods=["GET", "POST"])
@@ -56,7 +61,3 @@ def login():
 
     # 跳转到后台管理首页
     return redirect(url_for('admin.index'))
-
-
-
-
