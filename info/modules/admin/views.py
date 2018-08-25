6d84ad17b58dfdf8ce6db67c23cc41872d8b4c66
zws910 <vincentzheng0910@gmail.com>
2018-08-25 16:38:15 +0800

    admin login  -- if logged in jump to index directly

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -18,6 +18,12 @@ def index():
 def login():
     """管理员登录"""
     if request.method == "GET":
+        # 判断当前是否有登录, 如果有登录直接重定向到管理员后台主页
+        user_id = session.get("user_id", None)
+        is_admin = session.get("is_admin", False)
+        if user_id and is_admin:
+            return redirect(url_for('admin.index'))
+
         return render_template('admin/login.html')
 
     # 取到登录的参数
