222a1236308fc609b1cba2ad4ec9313e1dff1a88
zws910 <vincentzheng0910@gmail.com>
2018-08-25 21:07:56 +0800

    admin - news review

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -10,9 +10,45 @@ from flask import session
 from flask import url_for
 
 from info import constants
-from info.models import User
+from info.models import User, News
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
+
+
+@admin_blu.route('/news_review')
+def news_review():
+    """新闻审核"""
+
+    page = request.args.get("p", 1)
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page =1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = News.query.filter(News.status!=0).order_by(News.create_time.desc()).paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_review_dict())
+
+    data = {
+        "total_page": total_page,
+        "current_page": current_page,
+        "news_list": news_dict_list
+    }
+    return render_template('/admin/news_review.html', data=data)
+
 
 
 @admin_blu.route('/user_list')
