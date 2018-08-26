006807209673f0355d90d2b15a092f4a4c395820
zws910 <vincentzheng0910@gmail.com>
2018-08-26 10:48:49 +0800

    admin - news review detail finished

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime, timedelta
 
-from flask import current_app
+from flask import current_app, jsonify
 from flask import g
 from flask import redirect
 from flask import render_template
@@ -13,10 +13,46 @@ from info import constants
 from info.models import User, News
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@admin_blu.route('/news_review_action', methods=["GET", "POST"])
+def news_review_action():
+    """内容审核"""
+    # 1. 接受参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ("accept", "reject"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到数据")
+
+    if action == "accept":
+        news.status = 0
+    else:
+        reason = request.json.get("reason")
+        if not reason:
+            return jsonify(errno=RET.PARAMERR, errmsg="请输入拒绝原因")
+        news.status = -1
+        news.reason = reason
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @admin_blu.route('/news_review_detail/<int:news_id>')
 def news_review_detail(news_id):
+    """新闻审核详情"""
     news = None
 
     try:
